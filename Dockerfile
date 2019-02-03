7665596bc8ef56f205975ca5f875c95003521aa6
Iain Last <46655903+ijlast@users.noreply.github.com>
2019-02-03 18:10:34 +0100

    Develop (#3)
    * Fix to gradle wrapper properties
    
    * Added hot water temperature and return flow temperature to virtual
    device
    
    * Added Dockerfile
    
    * Update code comments and virtual device
    
    * Added docker build to gradle script

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM openjdk:8-jre-alpine
+
+# ALternative, however libtcnative-1 is not currently available for this version of springBoot (also, this is not intended to be a high volume server). 
+#FROM openjdk:8-jre-stretch
+#RUN apt-get update
+#RUN apt-get install -y libtcnative-1
+
+ADD luxtronic-*.jar /opt/luxtronic.jar
+
+ENV HEATPUMP_IP 192.168.178.6
+ENV HEATPUMP_PORT 8888
+EXPOSE 8080
+
+ENTRYPOINT ["java", "-jar", "/opt/luxtronic.jar"]
+
+# 
+# Example usage:
+# docker build -t luxtonic_rest_service .
+# docker run -dit --restart=always -e HEATPUMP_IP=<your heatpump ip> -e HEATPUMP_PORT=8889 -p 9090:8080 luxtonic_rest_service:latest
+#
+# N.B. HEATPUMP_PORT defaults to 8888
--- src/main/java/github/ijl/luxtronic/param/CoolingParameter.java
+++ src/main/java/github/ijl/luxtronic/param/CoolingParameter.java
@@ -1,16 +1,26 @@
 package github.ijl.luxtronic.param;
 
+import github.ijl.luxtronic.format.FormatConverter;
+import github.ijl.luxtronic.format.OneToOneConverter;
+
 public enum CoolingParameter {
-	PARAM_COOLING_OPERATION_MODE(108), COOLING_RELEASE_TEMP(110), COOLING_INLET_TEMP(132), COOLING_START(850),
-	COOLING_STOP(851);
+	PARAM_COOLING_OPERATION_MODE(108, OneToOneConverter.class), COOLING_RELEASE_TEMP(110, OneToOneConverter.class),
+	COOLING_INLET_TEMP(132, OneToOneConverter.class), COOLING_START(850, OneToOneConverter.class),
+	COOLING_STOP(851, OneToOneConverter.class);
 
 	private final Integer mValue;
+	private final Class<? extends FormatConverter> mFormatConverterClass;
 
-	private CoolingParameter(final Integer pValue) {
+	private CoolingParameter(final Integer pValue, final Class<? extends FormatConverter> pFormatConverterClass) {
 		mValue = pValue;
+		mFormatConverterClass = pFormatConverterClass;
 	}
 
 	public Integer getIntegerValue() {
 		return mValue;
+	}
+
+	public Class<? extends FormatConverter> getFormatConverterClass() {
+		return mFormatConverterClass;
 	}
 }
