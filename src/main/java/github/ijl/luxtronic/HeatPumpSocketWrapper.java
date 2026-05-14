7ddf6bdb0612ea03d86e1012d6acb77b08eb546b
Iain Last <ijlast@gmail.com>
2026-05-14 15:05:36 +0200

    Springboot and Java updates

--- src/main/java/github/ijl/luxtronic/HeatPumpSocketWrapper.java
+++ src/main/java/github/ijl/luxtronic/HeatPumpSocketWrapper.java
@@ -10,8 +10,8 @@ import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
